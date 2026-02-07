547a0eac4bf3c857f4059d0ad1a1f935693d0db4
agilan <agilans279@gmail.com>
2026-02-07 12:21:39 +0530

    Update API URL and deployment configs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from .views import get_facts, home
 
 urlpatterns = [
     path('', home),
     path('facts/', get_facts),
+    path('api/', include('api.urls')),
+
 ]
 
