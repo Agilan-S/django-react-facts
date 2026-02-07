cf01a7306c99278f0ec3a464d834e6a0b3f0fed7
agilan <agilans279@gmail.com>
2026-02-07 11:55:49 +0530

    Configure static files for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
 ]
 
 
+
 MIDDLEWARE = [
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
